72196eec837c6047ca56a8ba2ab80ca0b438d131
Nadar <nadarhilcuk@gmail.com>
2024-07-04 17:07:49 +0200

    text -

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var modal = document.getElementById('modal');
-//     var btn = document.querySelector('.rent-home__button');
-//     var closeBtn = document.getElementById('closeModal');
-
-//     btn.addEventListener('click', function() {
-//         modal.style.display = 'block';
-//     });
-
-//     closeBtn.addEventListener('click', function() {
-//         modal.style.display = 'none';
-//     });
-// });
-// Получаем элементы модалки и кнопку закрытия
 const modal = document.getElementById('modal');
 const closeModal = document.getElementById('closeModal');
 const openModalButton = document.querySelector('.rent-home__button');
